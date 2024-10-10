d9906c29fb8f362035309368c966f5db981f9971
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2024-10-10 15:08:23 -0300

    aaaaaaa

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -17,7 +17,20 @@ function App() {
   
   const [pantanalCard, setPantanalCard] = useState(Pantanal);
 
+  const [modalPtn, setModalPtn] = useState(true);
+
   const handleClick = () => setPantanalCard(PantanalLocked)
+  const handleModal = (num) => {
+    switch(num){
+
+      case 1:
+        setModalPtn((x) => !x)
+        break;
+      
+      default:
+        return 0;
+    }
+  }
 
   return (
     <div className="container">
@@ -28,7 +41,7 @@ function App() {
       modules={[EffectCards]}
       className="mySwiper"
       >
-        <SwiperSlide onClick={handleClick}>
+        <SwiperSlide onClick={() => handleModal(1)}>
           <img src={pantanalCard} alt="aaaaaa" />
         </SwiperSlide>
         <SwiperSlide>
@@ -54,9 +67,52 @@ function App() {
         </SwiperSlide>
       </Swiper>
 
-      <div className='modal'>
-        aaaaaaaaaa
-      </div>
+      {modalPtn && <div className='modal'>
+        <div className={`${pantanalCard === Pantanal ? 'pantanalUld' : 'pantanalLkd'} dentroModal`}>
+            <h2 className='ttlBioma'>PANTANAL</h2>
+            {pantanalCard === Pantanal ? (
+              <div>
+                <div className='descricaoBiomas'>
+                <p>O Pantanal é a maior planície alagada do mundo, localizado principalmente no Brasil, mas também se estendendo pelo Paraguai e Bolívia. Conhecido por sua rica biodiversidade, o Pantanal é um dos ecossistemas mais importantes do planeta.</p>
+                
+                <div className='itemsDesc'>
+                  <span className='ttlItem'>ÁREA</span>
+                  <span className='descItem'>884mil km²</span>
+                </div>
+
+                <div className='itemsDesc'>
+                  <span className='ttlItem'>REGIÕES</span>
+                  <span className='descItem'>Norte, Nordeste</span>
+                </div>
+
+                <div className='itemsDesc'>
+                  <span className='ttlItem'>FAUNA</span>
+                  <span className='descItem'>
+                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
+                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
+                    Tatu-Bola <span className='sub'>tatubolaaa</span>
+                  </span>
+                </div>
+
+                <div className='itemsDesc'>
+                  <span className='ttlItem'>FLORA</span>
+                  <span className='descItem'>
+                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
+                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
+                    Tatu-Bola <span className='sub'>tatubolaaa</span>
+                  </span>
+                </div>
+
+                </div>                
+                <div className='coor'></div>
+              </div>
+            ) : (
+              <p> Opa </p>
+            )}
+            
+            <button onClick={() => handleModal(1)} className='botaoModal'>x</button>
+        </div>
+      </div>}
     </div>
   )
 }
