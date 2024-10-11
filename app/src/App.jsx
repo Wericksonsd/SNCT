028af113fce2e4d76a6751cbbb87292221d4eaaf
Werickson Dauta <105117577+Wericksonsd@users.noreply.github.com>
2024-10-11 08:17:34 -0300

    dasd

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -169,64 +169,6 @@ function App() {
         </div>
       </div>}
 
-      {/* CAATINGA */}
-      {modalPtn && <div className='modal'>
-        <div className={`${pantanalCard === Pantanal ? 'pantanalUld' : 'pantanalLkd'} dentroModal`}>
-            <h2 className='ttlBioma'>PANTANAL</h2>
-            {pantanalCard === Pantanal ? (
-              <div>
-                <div className='descricaoBiomas'>
-                <p>O Pantanal é a maior planície alagada do mundo, localizado principalmente no Brasil, mas também se estendendo pelo Paraguai e Bolívia. Conhecido por sua rica biodiversidade, o Pantanal é um dos ecossistemas mais importantes do planeta.</p>
-                
-                <div className='itemsDesc'>
-                  <span className='ttlItem'>ÁREA</span>
-                  <span className='descItem'>884mil km²</span>
-                </div>
-
-                <div className='itemsDesc'>
-                  <span className='ttlItem'>REGIÕES</span>
-                  <span className='descItem'>Norte, Nordeste</span>
-                </div>
-
-                <div className='itemsDesc'>
-                  <span className='ttlItem'>FAUNA</span>
-                  <span className='descItem'>
-                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
-                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
-                    Tatu-Bola <span className='sub'>tatubolaaa</span>
-                  </span>
-                </div>
-
-                <div className='itemsDesc'>
-                  <span className='ttlItem'>FLORA</span>
-                  <span className='descItem'>
-                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
-                    Tatu-Bola <span className='sub'>tatubolaaa</span><br />
-                    Tatu-Bola <span className='sub'>tatubolaaa</span>
-                  </span>
-                </div>
-
-                </div>                
-                <div className='coor'></div>
-              </div>
-            ) : (
-              <div className='containerPergunta'>
-                <h2>PERGUNTA SERIA TENTA ACERTAR AI</h2>
-                <form onSubmit={handleSubmit}>
-                  <input type="number"
-                  placeholder='INSIRA O CÓDIGO'
-                  value={inputCod}
-                  onChange={handleInputChange}
-                  maxLength={5}
-                  id='inputPTN' />
-                  <button type='button' onClick={() => handleClick()}>ENVIAR</button>
-                </form>
-              </div>
-            )}
-            
-            <button onClick={() => handleModal(1)} className='botaoModal'>x</button>
-        </div>
-      </div>}
 
       {codCorreto && <div className='modalErro'>
           <div className='erroContainer'>
